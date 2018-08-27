9207057b9980e1a1a71820ab7f43ae74b1cd0105
vhcar <vhcar@VICTOR-PC>
2018-08-27 14:15:54 -0300

    Inicio

---
+++ Ex6_lista_3/Ex6_lista_3/Cliente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6_lista_3
+{
+    class Cliente
+    {
+        private string nome;
+        private long cpf;
+
+      
+        public void ListarContas()
+        {
+
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public long Cpf
+        {
+            get
+            {
+                return cpf;
+            }
+
+            set
+            {
+                cpf = (value <= 0) ? value : 0;
+            }
+        }
+
+    }
+
+}
---
+++ Ex6_lista_3/Ex6_lista_3/Conta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6_lista_3
+{
+    class Conta
+    {
+        private long numConta;
+        private long numAgencia;
+        private double saldo;
+        private double limite;
+        private int tempoConta;
+
+        public
+    }
+}
